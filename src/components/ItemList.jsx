1349fb159e3af72aafc9503524e9cfa1bbeefffc
Eliseo Casas Bossicovich <beliseocasas@gmail.com>
2022-11-24 16:34:30 -0300

    agrego una prop greeting

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,26 +1,31 @@
-const ItemList = () =>{
+const ItemList = ({titulo}) =>{
     return (
-        // section categorias
-        <section className="categorias__grid">
-            <article className="ofertas">
-                <a href="paginas/ofertas.html"><button className="categorias__boton" >Ofertas</button></a>
-            </article>
-            <article className="granja">
-                <a href="paginas/granja.html"><button className="categorias__boton">Granja</button></a>
-            </article>
-            <article className="delacasa">
-                <a href="paginas/delacasa.html"><button className="categorias__boton">De la casa</button></a>
-            </article>
-            <article className="verduleria">
-                <a href="paginas/verduleria.html"><button className="categorias__boton">Verdulería</button></a>
-            </article>
-            <article className="bebidas">
-                <a href="paginas/bebidas.html"><button className="categorias__boton">Bebidas</button></a>
-            </article>
-            <article className="congelados">
-                <a href="paginas/congelados.html"><button className="categorias__boton">Congelados</button></a>
-            </article>
-        </section>
+        <div>
+            <div>
+                <h1>{titulo}</h1>
+            </div>
+            {/* // section categorias */}
+            <section className="categorias__grid">
+                <article className="ofertas">
+                    <a href="paginas/ofertas.html"><button className="categorias__boton" >Ofertas</button></a>
+                </article>
+                <article className="granja">
+                    <a href="paginas/granja.html"><button className="categorias__boton">Granja</button></a>
+                </article>
+                <article className="delacasa">
+                    <a href="paginas/delacasa.html"><button className="categorias__boton">De la casa</button></a>
+                </article>
+                <article className="verduleria">
+                    <a href="paginas/verduleria.html"><button className="categorias__boton">Verdulería</button></a>
+                </article>
+                <article className="bebidas">
+                    <a href="paginas/bebidas.html"><button className="categorias__boton">Bebidas</button></a>
+                </article>
+                <article className="congelados">
+                    <a href="paginas/congelados.html"><button className="categorias__boton">Congelados</button></a>
+                </article>
+            </section>
+        </div>
         // fin section categorias
     );
 }
