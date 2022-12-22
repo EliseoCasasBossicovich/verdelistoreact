c7c2247b2f4a0d08a6563a6d6c96860289ee8c0c
Eliseo Casas Bossicovich <beliseocasas@gmail.com>
2022-12-22 14:20:58 -0300

    correccion de links a categorías

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -32,22 +32,25 @@ const Navbar = () =>{
                                 Categorias
                                 </a>
                                 <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <Link to ='/categoria/ofertas'>
+                                    <Link to ='/category/ofertas'>
                                         <li className="dropdown-item">Ofertas</li>
                                         </Link>
-                                    <Link to ='/categoria/delacasa'>
-                                        <li className="dropdown-item">De la Casa</li>
+                                    <Link to ='/category/empanadas'>
+                                        <li className="dropdown-item">Empanadas</li>
                                         </Link>
-                                    <Link to ='/categoria/verduleria'>
+                                        <Link to ='/category/canelones'>
+                                        <li className="dropdown-item">Canelones</li>
+                                        </Link>
+                                    <Link to ='/category/verduleria'>
                                         <li className="dropdown-item">Verdulería</li>
                                         </Link>
-                                    <Link to ='/categoria/granja'>
+                                    <Link to ='/category/granja'>
                                         <li className="dropdown-item">Granja</li>
                                     </Link>
-                                    <Link to ='/categoria/bebidas'>
+                                    <Link to ='/category/bebidas'>
                                         <li className="dropdown-item">Bebidas</li>
                                     </Link>
-                                    <Link to ='/categoria/congelados'>
+                                    <Link to ='/category/congelados'>
                                         <li className="dropdown-item">Congelados</li>
                                     </Link>
                                 </ul>
@@ -61,3 +64,67 @@ const Navbar = () =>{
 }
 
 export default Navbar;
+
+// import CartPedido from "./CartWidget.jsx";
+// import headerLogo from "../recursos/logovd.png";
+// import { Link } from "react-router-dom";
+
+// const Navbar = () =>{
+    
+//     return(
+//         <nav className="header">
+//             {/* logo*/}
+//             <Link to = '/'>
+//             <img className="header__lista--logo" alt="VerdeListo" src={headerLogo}/>
+//             </Link>
+//             {/* fin logo */}
+//             <nav className="navbar navbar-expand-lg header__nav color-header">
+//                 <div className="container-fluid header__cont">
+//                     <div className="p-2 flex-fill header__pedidos">
+//                         <CartPedido/>
+//                     </div>
+//                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+//                         <span className="navbar-toggler-icon"/>
+//                     </button>
+//                     <div className="collapse navbar-collapse header__collapse" id="navbarSupportedContent">
+//                         <div>
+//                             <form className="d-flex " role="search">
+//                                 <input className="form-control me-2 header__buscar" type="search" placeholder="Buscar..." aria-label="Search"/>
+//                                 <button className="btn btn-outline-success header__buscar--boton color-buscarboton" type="submit">Buscar</button>
+//                             </form>
+//                         </div>
+//                         <ul className="navbar-nav me-auto mb-2 mb-lg-0 p-2">
+//                             <li className="nav-item dropdown">
+//                                 <a className="nav-link dropdown-toggle fs-5 fw-semibold categoria__font" href="index.html" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+//                                 Categorias
+//                                 </a>
+//                                 <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+//                                     <Link to ='/categoria/ofertas'>
+//                                         <li className="dropdown-item">Ofertas</li>
+//                                         </Link>
+//                                     <Link to ='/categoria/delacasa'>
+//                                         <li className="dropdown-item">De la Casa</li>
+//                                         </Link>
+//                                     <Link to ='/categoria/verduleria'>
+//                                         <li className="dropdown-item">Verdulería</li>
+//                                         </Link>
+//                                     <Link to ='/categoria/granja'>
+//                                         <li className="dropdown-item">Granja</li>
+//                                     </Link>
+//                                     <Link to ='/categoria/bebidas'>
+//                                         <li className="dropdown-item">Bebidas</li>
+//                                     </Link>
+//                                     <Link to ='/categoria/congelados'>
+//                                         <li className="dropdown-item">Congelados</li>
+//                                     </Link>
+//                                 </ul>
+//                             </li>
+//                         </ul>
+//                     </div>
+//                 </div>
+//             </nav>
+//         </nav>
+//     );
+// }
+
+// export default Navbar;
--- src/pages/ItemListContainer.jsx
+++ src/pages/ItemListContainer.jsx
@@ -6,59 +6,58 @@ import ItemList from "../components/ItemList";
 
 const ItemListContainer = () =>{
     const [productos, setProductos] = useState([]);
-    const { id } = useParams();
+    const { categoryId} = useParams();
     useEffect(() => {
     const getProductos = new Promise(resolve => {
         setTimeout(() => {
             resolve(Data)
         }, 1000);
     });
-        if (id) {
+        if (categoryId) {
             getProductos.then(res => 
-                setProductos(res.filter((productos) => productos.id === id)));    
+                setProductos(res.filter((productos) => productos.category === categoryId)));    
         } else {
         getProductos.then((res) => setProductos(res))
         }
-    }, [id])
+    }, [categoryId])
     // console.log(productos);
     return (
         <div>
             {/* // section categorias */}
             <section className="categorias__grid">
                 <article className="ofertas">
-                    <Link to ='/categoria/ofertas'>
+                    <Link to ='/category/ofertas'>
                         <button className="categorias__boton" >Ofertas</button>
                     </Link>
                 </article>
                 <article className="granja">
-                    <Link to ='/categoria/granja'>
-                        <button className="categorias__boton">Granja</button>
+                    <Link to ='/category/empanadas'>
+                        <button className="categorias__boton">Empanadas</button>
                     </Link>
                 </article>
                 <article className="delacasa">
-                    <Link to ='/categoria/delacasa'>
-                        <button className="categorias__boton">De la casa</button>
+                    <Link to ='/category/canelones'>
+                        <button className="categorias__boton">Canelones</button>
                     </Link>
                 </article>
                 <article className="verduleria">
-                    <Link to ='/categoria/verduleria'>
+                    <Link to ='/category/verduleria'>
                         <button className="categorias__boton">Verdulería</button>
                     </Link>
                 </article>
                 <article className="bebidas">
-                    <Link to ='/categoria/bebidas'>
+                    <Link to ='/category/bebidas'>
                         <button className="categorias__boton">Bebidas</button>
                     </Link>
                 </article>
                 <article className="congelados">
-                    <Link to ='/categoria/congelados'>
+                    <Link to ='/category/congelados'>
                         <button className="categorias__boton">Congelados</button>
                     </Link>
                 </article>
             </section>
             <section>
-                <h1>{id}</h1>
-                {/* <ItemList productos={productos}/> */}
+                <h1>{categoryId}</h1>
             </section>
             <section className="row">
                 <ItemList productos={productos}/>
@@ -68,4 +67,75 @@ const ItemListContainer = () =>{
     );
 }
 export default ItemListContainer;
+
+// import { useState, useEffect } from "react";
+// import { useParams } from "react-router-dom";
+// import Data from "../productos.json";
+// import { Link } from "react-router-dom";
+// import ItemList from "../components/ItemList";
+
+// const ItemListContainer = () =>{
+//     const [productos, setProductos] = useState([]);
+//     const { id } = useParams();
+//     useEffect(() => {
+//     const getProductos = new Promise(resolve => {
+//         setTimeout(() => {
+//             resolve(Data)
+//         }, 1000);
+//     });
+//         if (id) {
+//             getProductos.then(res => 
+//                 setProductos(res.filter((productos) => productos.id === id)));    
+//         } else {
+//         getProductos.then((res) => setProductos(res))
+//         }
+//     }, [id])
+//     // console.log(productos);
+//     return (
+//         <div>
+//             {/* // section categorias */}
+//             <section className="categorias__grid">
+//                 <article className="ofertas">
+//                     <Link to ='/categoria/ofertas'>
+//                         <button className="categorias__boton" >Ofertas</button>
+//                     </Link>
+//                 </article>
+//                 <article className="granja">
+//                     <Link to ='/categoria/granja'>
+//                         <button className="categorias__boton">Granja</button>
+//                     </Link>
+//                 </article>
+//                 <article className="delacasa">
+//                     <Link to ='/categoria/delacasa'>
+//                         <button className="categorias__boton">De la casa</button>
+//                     </Link>
+//                 </article>
+//                 <article className="verduleria">
+//                     <Link to ='/categoria/verduleria'>
+//                         <button className="categorias__boton">Verdulería</button>
+//                     </Link>
+//                 </article>
+//                 <article className="bebidas">
+//                     <Link to ='/categoria/bebidas'>
+//                         <button className="categorias__boton">Bebidas</button>
+//                     </Link>
+//                 </article>
+//                 <article className="congelados">
+//                     <Link to ='/categoria/congelados'>
+//                         <button className="categorias__boton">Congelados</button>
+//                     </Link>
+//                 </article>
+//             </section>
+//             <section>
+//                 <h1>{id}</h1>
+//                 {/* <ItemList productos={productos}/> */}
+//             </section>
+//             <section className="row">
+//                 <ItemList productos={productos}/>
+//             </section>
+//         </div>
+//         // fin section categorias
+//     );
+// }
+// export default ItemListContainer;
 
